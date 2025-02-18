726c22c7f49f87c8f5092910c53a3db538d18a3b
AlejndroSG <alex2005sg@gmail.com>
2025-02-18 10:50:37 +0100

    2025-02-18

--- React+Tailwind/src/pages/Detalles.jsx
+++ React+Tailwind/src/pages/Detalles.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 const Detalles = () => {
+  const {id} = useParams();
+  console.log(id);
+  const [producto, setProducto] = useState([]);
+  
+  useEffect( () => {
+    fetch(`https://dummyjson.com/products/${id}`)
+    .then(respuesta => respuesta.json())
+    .then(datos => setProducto(datos));
+  }, []);
+  console.log(producto);
   return (
-    <div>Detalles</div>
+    <>
+      <div className='grid grid-cols-3 gap-4'>
+            {productos.map(producto =>(
+                <div className='bg-white p-5 rounded-lg shadow-md' key={producto.id} >
+                    <img src={producto.images[0]} alt="" />
+                    <h2>{producto.title}</h2>
+                    <p>{producto.description}</p>
+                    <button className='p-5 bg-amber-400 rounded-2xl mt-5' ><Link to={`${producto.id}`}>Ver mas</Link></button>
+                </div>
+            ))}
+        </div>
+    </>
   )
 }
 
--- React+Tailwind/src/pages/Home.jsx
+++ React+Tailwind/src/pages/Home.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+  const [productos, setProductos] = useState([]);
+    useEffect( () => {
+      fetch(`https://dummyjson.com/products`)
+      .then(respuesta => respuesta.json())
+      .then(datos => setProductos(datos.products));
+    }, []);
+    console.log(productos);
   return (
-    <div>Home</div>
+    <>
+      <div className='grid grid-cols-3 gap-4'>
+        {productos.map(producto =>(
+            producto.id <= 10 ?
+            <div className='bg-white p-5 rounded-lg shadow-md' key={producto.id} >
+                <img src={producto.images[0]} alt="" />
+                <h2>{producto.title}</h2>
+                <p>{producto.description}</p>
+                {/* <button className='p-5 bg-amber-400 rounded-2xl mt-5' ><Link to={`${producto.id}`}>Ver mas</Link></button> */}
+                {console.log(producto)}
+            </div>
+            : null
+        ))}
+      </div>
+    </>
   )
 }
 
